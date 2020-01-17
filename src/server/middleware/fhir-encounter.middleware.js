2a3f242e7eae369c18c7bf3ab52c94f79f471e0a
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-17 11:34:19 +0000

    feat: convert admission_date  to select input

--- src/server/middleware/fhir-encounter.middleware.js
+++ src/server/middleware/fhir-encounter.middleware.js
@@ -10,7 +10,10 @@ module.exports = function fhirEncounterMiddleware(config) {
 	return async (req, res, next) => {
 		// Retrieve data from Encounter FHIR endpoint
 		let searchPath = '';
-		if (req.patient_nhsNo !== '' && typeof req.patient_nhsNo !== 'undefined') {
+		if (
+			req.patient_nhsNo !== '' &&
+			typeof req.patient_nhsNo !== 'undefined'
+		) {
 			searchPath = `${config.url}Encounter?patient.identifier=https://fhir.nhs.uk/Id/nhs-number|${req.patient_nhsNo}&class=inpatient`;
 		} else if (
 			req.customparams.patient_mrn !== '' &&
@@ -32,82 +35,97 @@ module.exports = function fhirEncounterMiddleware(config) {
 					// Only parse inpatient encounter resources
 					if (element.resource.class.code === 'IMP') {
 						// Retrieve admitting specialty
-						element.resource.type.forEach((specialtyType) => {
-							specialtyType.coding.forEach(
-								(specialtyTypeCode) => {
-									if (
-										specialtyTypeCode.system ===
-										'https://fhir.nhs.uk/STU3/CodeSystem/DCH-Specialty-1'
-									) {
-										inpatEncounter.admission_specialty =
-											specialtyTypeCode.display;
+						if (element.resource.type) {
+							element.resource.type.forEach((specialtyType) => {
+								specialtyType.coding.forEach(
+									(specialtyTypeCode) => {
+										if (
+											specialtyTypeCode.system ===
+											'https://fhir.nhs.uk/STU3/CodeSystem/DCH-Specialty-1'
+										) {
+											inpatEncounter.admission_specialty =
+												specialtyTypeCode.display;
+										}
 									}
-								}
-							);
-						});
+								);
+							});
+						}
+
 
 						inpatEncounter.admission_date =
 							element.resource.period.start;
-                        inpatEncounter.discharge_date =
-                            element.resource.period.end;
+						inpatEncounter.discharge_date =
+							element.resource.period.end;
 						// Retrieve admission and discharge method
-						element.resource.hospitalization.extension.forEach(
-							(hospitalizationExtension) => {
-								if (
-									hospitalizationExtension.url ===
-									'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1'
-								) {
-									inpatEncounter.admission_method =
-										hospitalizationExtension.valueCodeableConcept.coding[0].display;
-                                }
-                                
-								if (
-									hospitalizationExtension.url ===
-									'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1'
-								) {
-									inpatEncounter.discharge_method =
-										hospitalizationExtension.valueCodeableConcept.coding[0].display;
-                                }
-							}
-						);
+						if (
+							element.resource.hospitalization &&
+							element.resource.hospitalization.extension
+						) {
+							element.resource.hospitalization.extension.forEach(
+								(hospitalizationExtension) => {
+									if (
+										hospitalizationExtension.url ===
+										'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1'
+									) {
+										inpatEncounter.admission_method =
+											hospitalizationExtension.valueCodeableConcept.coding[0].display;
+									}
+
+									if (
+										hospitalizationExtension.url ===
+										'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1'
+									) {
+										inpatEncounter.discharge_method =
+											hospitalizationExtension.valueCodeableConcept.coding[0].display;
+									}
+								}
+							);
+						}
 
 						// Retrieve admitting and discharging consultant
-						element.resource.participant.forEach(
-							(participation) => {
-								participation.type.forEach(
-									(participationType) => {
-										participationType.coding.forEach(
-											(participationTypeCode) => {
-												if (
-													participationTypeCode.code ===
-													'ADM'
-												) {
-													inpatEncounter.admission_careProvider =
-														participation.individual.display;
-                                                }
-                                                
-                                                if (
-													participationTypeCode.code ===
-													'DIS'
-												) {
-													inpatEncounter.discharge_careProvider =
-														participation.individual.display;
-												}
-											}
-										);
-									}
-								);
-							}
-						);
+						if (element.resource.participant) {
+							element.resource.participant.forEach(
+								(participation) => {
+									participation.type.forEach(
+										(participationType) => {
+											participationType.coding.forEach(
+												(participationTypeCode) => {
+													if (
+														participationTypeCode.code ===
+														'ADM'
+													) {
+														inpatEncounter.admission_careProvider =
+															participation.individual.display;
+													}
 
-						inpatEncounter.admission_source =
-							element.resource.hospitalization.admitSource.coding[0].display;
+													if (
+														participationTypeCode.code ===
+														'DIS'
+													) {
+														inpatEncounter.discharge_careProvider =
+															participation.individual.display;
+													}
+												}
+											);
+										}
+									);
+								}
+							);
+						}
+
+						if (
+							element.resource.hospitalization &&
+							element.resource.hospitalization.admitSource
+						) {
+							inpatEncounter.admission_source =
+								element.resource.hospitalization.admitSource.coding[0].display;
+						}
 
 						params.push(inpatEncounter);
 					}
 				});
 
-				req.encounterresources = params;
+				req.customparams.encounterresources = JSON.stringify(params);
 				next();
 			})
 			.catch((err) => {
