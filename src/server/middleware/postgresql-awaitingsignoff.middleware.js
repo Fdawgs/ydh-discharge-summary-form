e8cc7f25f85e666626c11ebed94b2f9cf1c7e897
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-21 13:24:04 +0000

    refactor: replace case statement with object

--- src/server/middleware/postgresql-awaitingsignoff.middleware.js
+++ src/server/middleware/postgresql-awaitingsignoff.middleware.js
@@ -1,47 +1,37 @@
 /**
  * @author Frazer Smith
+ * @description Query PostgreSQL DB for discharge summaries awaiting sign-offs.
+ *
  * @param {Object} pool - Pool object.
  * @param {('dr'|'nurse'|'pharmacy')} signOffType - String specifying type
  * of sign-off that is still needed.
  * @return {Function} express middleware
- * @description Query PostgreSQL DB for discharge summaries awaiting sign-offs.
  */
 module.exports = function postgresqlAwaitingSignOffMiddleware(
 	pool,
 	signOffType
 ) {
 	return async (req, res, next) => {
-		// Build WHERE clause predicates
-		let predicateParts;
-		switch (signOffType.toLocaleLowerCase()) {
-			case 'nurse':
-				predicateParts =
-					"AND (raw::jsonb ? 'signoff_nurseFirstCheck' = FALSE OR raw::jsonb ? 'signoff_nurseSecondCheck' = FALSE)";
-				break;
-			case 'dr':
-				predicateParts =
-					"AND (raw::jsonb ? 'signoff_drCheckbox' = FALSE)";
-				break;
-			case 'pharmacy':
-				predicateParts =
-					"AND (raw::jsonb ? 'medication_overallPharmacySignOffCheck' = FALSE) AND (json_array_length(raw->'ttos') IS NOT NULL)";
-				break;
-			default:
-				break;
-		}
+		const predicateParts = {
+			nurse:
+				"(raw::jsonb ? 'signoff_nurseFirstCheck' = FALSE OR raw::jsonb ? 'signoff_nurseSecondCheck' = FALSE)",
+			dr: "(raw::jsonb ? 'signoff_drCheckbox' = FALSE)",
+			pharmacy:
+				"(raw::jsonb ? 'medication_overallPharmacySignOffCheck' = FALSE) AND (json_array_length(raw->'ttos') IS NOT NULL)"
+		};
 
 		const getInProgress = () =>
 			pool
 				.query(
 					`WITH CTE
-												  AS (SELECT *,
-															 ROW_NUMBER() OVER (PARTITION BY ID ORDER BY VERSION DESC) AS rn
-														FROM public.discharge_summary)
-											  SELECT id, "lastUpdate", raw, version
-												FROM CTE
-											   WHERE rn = 1
-											   ${predicateParts}
-											ORDER BY id DESC`
+					AS (SELECT *,
+					ROW_NUMBER() OVER (PARTITION BY ID ORDER BY VERSION DESC) AS rn
+					FROM public.discharge_summary)
+					SELECT id, "lastUpdate", raw, version
+					FROM CTE
+					WHERE rn = 1
+					AND ${predicateParts[signOffType]}
+					ORDER BY id DESC`
 				)
 				.then((response) => response.rows);
 
