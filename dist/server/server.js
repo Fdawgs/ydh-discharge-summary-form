942afc3ced0538cbef188f7b516da6c44bfedb49
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-31 08:47:34 +0000

    chore: prep release

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,46 +1,55 @@
-"use strict";function asyncGeneratorStep(a,b,c,d,e,f,g){try{var h=a[f](g),i=h.value}catch(a){return void c(a)}h.done?b(i):Promise.resolve(i).then(d,e)}function _asyncToGenerator(a){return function(){var b=this,c=arguments;return new Promise(function(d,e){function f(a){asyncGeneratorStep(h,d,e,f,g,"next",a)}function g(a){asyncGeneratorStep(h,d,e,f,g,"throw",a)}var h=a.apply(b,c);f(void 0)})}}function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}function _defineProperties(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,"value"in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}function _createClass(a,b,c){return b&&_defineProperties(a.prototype,b),c&&_defineProperties(a,c),a}var ActiveDirectory=require("activedirectory"),bodyParser=require("body-parser"),compression=require("compression"),cookieParser=require("cookie-parser"),express=require("express"),fs=require("fs"),helmet=require("helmet"),https=require("https"),http=require("http"),path=require("path"),_require=require("pg"),Pool=_require.Pool,retrieveInProgress=require("./middleware/postgresql-inprogress.middleware"),retrieveAwaitSignOff=require("./middleware/postgresql-awaitingsignoff.middleware"),fhirPatientRecord=require("./middleware/fhir-patient.middleware"),fhirAllergyRecord=require("./middleware/fhir-allergyintolerance.middleware"),fhirEncounterRecord=require("./middleware/fhir-encounter.middleware"),adGroupMembership=require("./middleware/ad-authorization.middleware"),insertUpdateRecord=require("./middleware/postgresql-upsert.middleware"),gatherCustomParams=require("./middleware/gather-custom-params.middleware"),Server=/*#__PURE__*/function(){/**
+"use strict";function asyncGeneratorStep(gen,resolve,reject,_next,_throw,key,arg){try{var info=gen[key](arg);var value=info.value}catch(error){reject(error);return}if(info.done){resolve(value)}else{Promise.resolve(value).then(_next,_throw)}}function _asyncToGenerator(fn){return function(){var self=this,args=arguments;return new Promise(function(resolve,reject){var gen=fn.apply(self,args);function _next(value){asyncGeneratorStep(gen,resolve,reject,_next,_throw,"next",value)}function _throw(err){asyncGeneratorStep(gen,resolve,reject,_next,_throw,"throw",err)}_next(undefined)})}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}function _defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor)}}function _createClass(Constructor,protoProps,staticProps){if(protoProps)_defineProperties(Constructor.prototype,protoProps);if(staticProps)_defineProperties(Constructor,staticProps);return Constructor}var ActiveDirectory=require("activedirectory");var bodyParser=require("body-parser");var compression=require("compression");var cookieParser=require("cookie-parser");var express=require("express");var fs=require("fs");var helmet=require("helmet");var https=require("https");var http=require("http");var path=require("path");var _require=require("pg"),Pool=_require.Pool;var retrieveInProgress=require("./middleware/postgresql-inprogress.middleware");var retrieveAwaitSignOff=require("./middleware/postgresql-awaitingsignoff.middleware");var fhirPatientRecord=require("./middleware/fhir-patient.middleware");var fhirAllergyRecord=require("./middleware/fhir-allergyintolerance.middleware");var fhirEncounterRecord=require("./middleware/fhir-encounter.middleware");var adGroupMembership=require("./middleware/ad-authorization.middleware");var insertUpdateRecord=require("./middleware/postgresql-upsert.middleware");var gatherCustomParams=require("./middleware/gather-custom-params.middleware");var Server=/*#__PURE__*/function(){/**
 	 * @param {Object} config - Server configuration values.
-	 */function a(){var b=0<arguments.length&&void 0!==arguments[0]?arguments[0]:{};// return self for chaining
-return _classCallCheck(this,a),this.config=b,this.app=express(),this.app.set("view engine","ejs"),this.app.use(express["static"](path.join(__dirname,"../public"))),this.app.set("views",path.join(__dirname,"../views")),this}/**
+	 */function Server(){var config=arguments.length>0&&arguments[0]!==undefined?arguments[0]:{};_classCallCheck(this,Server);this.config=config;// Setup our express instance
+this.app=express();// ejs settings
+this.app.set("view engine","ejs");this.app.use(express["static"](path.join(__dirname,"../public")));this.app.set("views",path.join(__dirname,"../views"));// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Handles FHIR API calls.
 	 * @param {Object} fhirConconfig
-	 */return _createClass(a,[{key:"configureFhirRouting",value:function configureFhirRouting(a){// return self for chaining
-return this.app.get("/searchpatient",fhirPatientRecord(a),// Only fetch patient data once
-function(a,b){b.render("./pages/patient_confirmation",a.patientresource)}),this.app.post("/createnew",gatherCustomParams(),fhirAllergyRecord(a),insertUpdateRecord(this.pool),retrieveInProgress(this.pool),/*#__PURE__*/function(){var a=_asyncToGenerator(/*#__PURE__*/regeneratorRuntime.mark(function a(b,c){return regeneratorRuntime.wrap(function(a){for(;;)switch(a.prev=a.next){case 0:c.render("./pages/patient_landing",b.customparams);case 1:case"end":return a.stop();}},a)}));return function(){return a.apply(this,arguments)}}()),this}/**
+	 */_createClass(Server,[{key:"configureFhirRouting",value:function configureFhirRouting(fhirConconfig){// search for patient details on FHIR endpoint to attempt to auto-populate fields in forms
+this.app.get("/searchpatient",fhirPatientRecord(fhirConconfig),// Only fetch patient data once
+function(req,res){res.render("./pages/patient_confirmation",req.patientresource)});this.app.post("/createnew",gatherCustomParams(),fhirAllergyRecord(fhirConconfig),insertUpdateRecord(this.pool),retrieveInProgress(this.pool),/*#__PURE__*/function(){var _ref=_asyncToGenerator(/*#__PURE__*/regeneratorRuntime.mark(function _callee(req,res){return regeneratorRuntime.wrap(function _callee$(_context){while(1){switch(_context.prev=_context.next){case 0:res.render("./pages/patient_landing",req.customparams);case 1:case"end":return _context.stop();}}},_callee)}));return function(_x,_x2){return _ref.apply(this,arguments)}}());// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Sets Helmet options for server.
-	 */},{key:"configureHelmet",value:function configureHelmet(a){// return self for chaining
-return this.app.use(helmet(a)),this}/**
+	 */},{key:"configureHelmet",value:function configureHelmet(helmetConfig){// Use Helmet to set response headers
+this.app.use(helmet(helmetConfig));// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Sets middleware options for server.
-	 */},{key:"configureMiddleware",value:function configureMiddleware(){// return self for chaining
-return this.app.use(compression({level:9})),this.app.use(bodyParser.urlencoded({extended:!1})),this.app.use(bodyParser.json()),this.app.use(cookieParser()),this.app.disable("x-powered-by"),this.app.use(adGroupMembership(this.ad)),this}/**
+	 */},{key:"configureMiddleware",value:function configureMiddleware(){this.app.use(compression({level:9}));this.app.use(bodyParser.urlencoded({extended:false}));this.app.use(bodyParser.json());this.app.use(cookieParser());this.app.disable("x-powered-by");// Check AD for authorization
+this.app.use(adGroupMembership(this.ad));// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @param {Object} config - Active Directory configuration values.
-	 */},{key:"configureActiveDirectory",value:function configureActiveDirectory(a){// return self for chaining
-return this.ad=new ActiveDirectory(a),this}/**
+	 */},{key:"configureActiveDirectory",value:function configureActiveDirectory(activeDirectoryConfig){this.ad=new ActiveDirectory(activeDirectoryConfig);// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Creates PostgreSQL connection pool for server.
 	 * @param {Object} config - PostgreSQL instance and connection settings.
-	 */},{key:"configurePostgresqlgres",value:function configurePostgresqlgres(a){// return self for chaining
-return this.pool=new Pool(a),this}/**
+	 */},{key:"configurePostgresqlgres",value:function configurePostgresqlgres(postgresqlConfig){this.pool=new Pool(postgresqlConfig);// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Sets routing options for navigation through server.
 	 * POST requests are used as opposed to GET when requesting new pages
 	 * in order to mask passed parameters in the URI from users.
-	 */},{key:"configureRouting",value:function configureRouting(a){// return self for chaining
-return this.app.get("/",gatherCustomParams(),retrieveAwaitSignOff(this.pool,"dr",void 0),retrieveAwaitSignOff(this.pool,"nurse",void 0),retrieveAwaitSignOff(this.pool,"pharmacy",void 0),retrieveAwaitSignOff(this.pool,"any",void 0),function(a,b){b.render("./pages/home",a)}),this.app.get("/contributors",function(a,b){b.render("./pages/contributors")}),this.app.get("*",function(a,b){b.redirect("/")}),this.app.post("/discharge_summary",gatherCustomParams(),fhirEncounterRecord(a),function(a,b){b.render("./pages/discharge_summary",a.customparams)}),this.app.post("/patient",gatherCustomParams(),retrieveInProgress(this.pool),function(a,b){b.render("./pages/patient_landing",a.customparams)}),this.app.post("/continue",gatherCustomParams(),fhirEncounterRecord(a),insertUpdateRecord(this.pool),function(a,b){b.render("./pages/discharge_summary",a.customparams)}),this.app.post("/preview",gatherCustomParams(),function(a,b,c){var d={id:a.customparams.row_id,version:a.customparams.version};a.powerbi=d,c()},insertUpdateRecord(this.pool),function(a,b){b.render("./pages/preview",a.powerbi)}),this}/**
+	 */},{key:"configureRouting",value:function configureRouting(fhirConconfig){this.app.get("/",gatherCustomParams(),retrieveAwaitSignOff(this.pool,"dr",undefined),retrieveAwaitSignOff(this.pool,"nurse",undefined),retrieveAwaitSignOff(this.pool,"pharmacy",undefined),retrieveAwaitSignOff(this.pool,"any",undefined),function(req,res){res.render("./pages/home",req)});this.app.get("/contributors",function(req,res){res.render("./pages/contributors")});/**
+		 * Stop users from attempting to GET any other page besides searchpatient. Since route and
+		 * searchpatient GETs are defined before this, they won't be caught and redirected by this.
+		 */this.app.get("*",function(req,res){res.redirect("/")});this.app.post("/discharge_summary",gatherCustomParams(),fhirEncounterRecord(fhirConconfig),function(req,res){res.render("./pages/discharge_summary",req.customparams)});this.app.post("/patient",gatherCustomParams(),retrieveInProgress(this.pool),function(req,res){res.render("./pages/patient_landing",req.customparams)});// Result of posting discharge summary form
+this.app.post("/continue",gatherCustomParams(),fhirEncounterRecord(fhirConconfig),insertUpdateRecord(this.pool),function(req,res){res.render("./pages/discharge_summary",req.customparams)});this.app.post("/preview",gatherCustomParams(),function(req,res,next){var powerBi={id:req.customparams.row_id,version:req.customparams.version};req.powerbi=powerBi;next()},insertUpdateRecord(this.pool),function(req,res){res.render("./pages/preview",req.powerbi)});// return self for chaining
+return this}/**
 	 * @author Frazer Smith
 	 * @description Start the server.
 	 * @param {string} port - Port for server to listen on.
-	 */},{key:"listen",value:function listen(a,b){var c=this.config;// Update the express app to be an instance of createServer
-if(!0===c.https){var d={};// Attempt to use PFX file if present
-c.ssl.pfx.pfx?(d.pfx=fs.readFileSync(c.ssl.pfx.pfx),d.passphrase=c.ssl.pfx.passphrase):(d.cert=fs.readFileSync(c.ssl.cert),d.key=fs.readFileSync(c.ssl.key)),this.app=https.createServer(d,this.app),this.config.protocol="https"}else this.config.protocol="http",this.app=http.createServer(this.app);// Start the app
-// console.log(`${server.name} listening for requests at ${this.config.protocol}://127.0.0.1:${port}`);
+	 */},{key:"listen",value:function listen(port,callback){var server=this.config;// Update the express app to be an instance of createServer
+if(server.https===true){var options={};// Attempt to use PFX file if present
+if(server.ssl.pfx.pfx){options.pfx=fs.readFileSync(server.ssl.pfx.pfx);options.passphrase=server.ssl.pfx.passphrase}else{options.cert=fs.readFileSync(server.ssl.cert);options.key=fs.readFileSync(server.ssl.key)}this.app=https.createServer(options,this.app);this.config.protocol="https"}else{this.config.protocol="http";this.app=http.createServer(this.app)}// Start the app
+this.app.listen(port,callback);// console.log(`${server.name} listening for requests at ${this.config.protocol}://127.0.0.1:${port}`);
 // return self for chaining
-return this.app.listen(a,b),this}/**
+return this}/**
 	 * @author Frazer Smith
 	 * @description Shut down server (non-gracefully).
-	 */},{key:"close",value:function close(){// return self for chaining
-return this.pool.end(),this.app.close(),this}}]),a}();module.exports=Server;
+	 */},{key:"close",value:function close(){this.pool.end();this.app.close();// return self for chaining
+return this}}]);return Server}();module.exports=Server;
--- docs/roadmap.md
+++ docs/roadmap.md
@@ -1,48 +1,30 @@
 # Roadmap
 
-## v0.18.0
+## v0.19.0
 
 ### Discharge Summary
 
 #### Admission Tab
--   Add 'Weight on Admission' field name
-    -   Add numeric input
-    
-#### Clinical Tab
-
--   Revise Advanced Care Section (with input from Palliative Care Nurse Consultant)
+-   Autopopulate 'Admitting Ward' input from TrakCare (requires additions to YDH's FHIR Encounter Resource API)
 
 #### Therapy and Care Arrangements Tab
 
--   Add Safeguarding section
-    -   Add 'Have social services been involved in the discharge?' field name
-        -   Add mandatory select input with options of '', 'No', 'Yes'
-        -   If 'Yes' selected then present textbox input
-    -   Add 'Was there a Deprivation of Liberty (DOL) during this admission?' field name
-        -   Add mandatory select input with options of '', 'No', 'Yes'    
-
--   Add Care Arrangements Section (with input from Therapy Lead)
--   Add Mobility Section (with input from Therapy Lead)
--   Add Continence Section (with input from Therapy Lead)
--   Add Dressings Section (with input from Therapy Lead)
+-   Revise Therapy and Care Arrangements Tab with input from Therapy Lead
 
 #### Discharge Tab
--   Add 'Weight on Discharge' field name
-    -   Add numeric input
 
-
-
-## v0.19.0
-
-#### Discharge Tab
 -   Add 'Paediatric Discharge Notes' fieldname and text area input that appears if a patient is under the age of 18
--   Revise Discharge Specialty list
+-   Autopopulate 'Discharging Specialty' input from TrakCare (requires additions to YDH's FHIR Encounter Resource API)
+-   Autopopulate 'Discharging Ward' input from TrakCare (requires additions to YDH's FHIR Encounter Resource API)
 
 ### Patient Landing Page
 
 -   Add 'Recently Sent' section
 
 ## v1.0.0 - Live Release
+
+### Discharge Summary
+-   Restrict editing of input fields to certain user groups (i.e. only a nurse can tick the nurse sign off boxes)
 
 ### Home page
 
@@ -60,9 +42,8 @@
 ### Sitewide
 
 -   Assign suitable aria attributes to elements for accessibility purposes
--   Replace EJS and raw JavaScript with either AngularJS or ReactJS frameworks
+-   Replace EJS and raw JavaScript with either AngularJS, ReactJS, or VueJS frameworks
 
 # Known Issues
 
 -   OOH switching to allow discharges to be sent between 17:15 and 33:00 (Monday - Friday) without patient sign off not in place
-    -   Will break with BST/GMT changes
